760f86929394ea999f6191ed0d7fcfb11eef3326
Juan Pablo Heis <80589629+juanpabloheis@users.noreply.github.com>
2021-12-02 20:12:35 -0300

    update footer

--- client/src/Componentes/Footer/Footer.jsx
+++ client/src/Componentes/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import styles from './Footer.module.css'
 export default function Footer() {
     return (
         <div className={styles.container}>
-            <p>by Dev. Juan Pablo Heis |</p>
+            <p>Created by Dev. Juan Pablo Heis |</p>
             <div className={styles.icons}>
                 <a href="https://www.linkedin.com/in/juanpabloheis/" target="_blank"  rel="noreferrer"><BsLinkedin />  </a>
                 <a href="https://github.com/juanpabloheis/Dogs" target="_blank" rel="noreferrer"><BsGithub /></a>
